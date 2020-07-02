53020db3acc0d9b6d0f95f41001dee0352edf610
Gideon Okuro <gideonollonde@gmail.com>
2020-07-02 12:29:09 +0300

    second commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,57 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
+import models.Post;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
-import sun.jvm.hotspot.runtime.StaticBaseConstructor;
 
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+        //get: show new post form
+        get("/posts/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "newpost-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //post: process new post form
+        post("/post/new", (request, response) -> {//URL to make new post on POST route
+            Map<String, Object> model = new HashMap<String, Object>();
+            String content = request.queryParams("content");
+            Post newPost = new Post(content);
+            model.put("post", newPost);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get: show all posts
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Post> posts = Post.getAll();
+            model.put("posts", posts);
+
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/posts/:id", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfPostToFind = Integer.parseInt(req.params(":id")); //pull id - must match route segment
+            Post foundPost = Post.findById(idOfPostToFind); //use it to find post
+            model.put("post", foundPost); //add it to model for template to display
+            return new ModelAndView(model, "post-detail.hbs"); //individual post page.
+        }, new HandlebarsTemplateEngine());
+
+        //get: show an individual post
+
+        //get: show a form to update a post
+
+        //post: process a form to update a post
+
+        //get: delete an individual post
+
+        //get: delete all posts
     }
 }
--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -3,8 +3,8 @@ package models;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class PostTest {
 
@@ -20,4 +20,59 @@ public class PostTest {
         assertEquals("Day 1: Intro", post.getContent());
     }
 
+    @After
+    public void tearDown() {
+        Post.clearAllPosts(); //clear out all the posts before each test.
+    }
+
+    @Test
+    public void AllPostsAreCorrectlyReturned_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertEquals(2, Post.getAll().size());
+    }
+
+    @Test
+    public void AllPostsContainsAllPosts_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertTrue(Post.getAll().contains(post));
+        assertTrue(Post.getAll().contains(otherPost));
+    }
+
+    @Test
+    public void getPublished_isFalseAfterInstantiation_false() throws Exception {
+        Post myPost = new Post("Day 1: Intro");
+        assertEquals(false, myPost.getPublished()); //should never start as published
+    }
+
+    @Test
+    public void getCreatedAt_instantiatesWithCurrentTime_today() throws Exception{
+        Post myPost = setupNewPost();
+        assertEquals(LocalDateTime.now().getDayOfWeek(), myPost.getCreatedAt().getDayOfWeek());
+    }
+
+    @Test
+    public void getId_postsInstantiatesWithAnID_1() throws Exception{
+        Post.clearAllPosts();
+        Post myPost = new Post("Day 1: Intro");
+        assertEquals(1, myPost.getId());
+    }
+
+    public Post setupNewPost(){
+        return new Post("Day 1: Intro");
+    }
+
+    @Test
+    public void findReturnsCorrectPost() throws Exception {
+        Post post = setupNewPost();
+        assertEquals(1, Post.findById(post.getId()).getId());
+    }
+
+    @Test
+    public void findReturnsCorrectPostWhenMoreThanOnePostExists() throws Exception {
+        Post post = setupNewPost();
+        Post otherPost = new Post("How to pair successfully");
+        assertEquals(2, Post.findById(otherPost.getId()).getId());
+    }
 }
